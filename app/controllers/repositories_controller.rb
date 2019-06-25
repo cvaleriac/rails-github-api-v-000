e8c9854e028b49531cf07a81630f9407fc8f575f
valeria campilongo <cvaleriac@hotmail.it>
2019-06-25 18:55:40 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -2,14 +2,14 @@ class RepositoriesController < ApplicationController
   def index
     resp = Faraday.get("https://api.github.com/user/repos") do |req|
       req.headers['Authorization'] = "token #{session[:token]}"
-      req.headers['Accept'] = 'application/json' 
+      req.headers['Accept'] = 'application/json'
     end
     @repos = JSON.parse(resp.body)
   end
 
   def create
     resp = Faraday.post("https://api.github.com/user/repos") do |req|
-      req.headers['Authorization'] = "token #{session[:token]}" 
+      req.headers['Authorization'] = "token #{session[:token]}"
       req.headers['Accept'] = 'application/json'
       req.body = {name: params[:name]}.to_json
     end
